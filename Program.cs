f5ac84e2335112df4325a67bdafa8b133be64ffe
KalphosZeromar <74295535+KalphosZeromar@users.noreply.github.com>
2024-07-01 21:36:57 +0100

    end of night push

--- Program.cs
+++ Program.cs
@@ -5,11 +5,28 @@ class index
     public static void Main(String[] args)
     {
         mathcore math = new mathcore();
-        int n;
+        int n = 0, x = 5, y = 4, z = 10;
         while (true)
         {
+            Console.WriteLine("Which choice would you like to make.");
+            n = Convert.ToInt32(Console.ReadLine());
             switch (n)
             {
+                case 0:
+                    Console.WriteLine("Thank you for using this program.");
+                    break;
+                case 1:
+                    Console.WriteLine("Shoot = ", z * math.shoot(x/6,y/6));
+                    break;
+                case 2:
+                    Console.WriteLine("Shoot rr hit = ", z*math.shootplusrrhit(x/6,y/6));
+                    break;
+                case 3:
+                    Console.WriteLine("Shoot rr wound = ", z*math.shootplusrrwound(n/6,y/6));
+                    break;
+                case 4:
+                    Console.WriteLine("Shoot rr full = ", z*math.shootplusrrfull(n/6,y/6));
+                    break;
                 default:
                     break;
             }
