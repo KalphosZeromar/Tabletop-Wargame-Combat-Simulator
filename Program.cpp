01b53e3ed7e484d1caa3bf29c53bfbe813523f24
Joseph Joshua Cowley <bobzillagonagon@gmail.com>
2025-05-20 00:59:55 +0100

    non functional tester but some progress

--- Program.cpp
+++ Program.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
-class::program()
+#include "mathcore.h"
+using namespace std;
+int main()
 {
-    
+    int n, x=1, y=1;
+    cout << "Please enter an integer between 1-4";
+    cin >> n;
+    switch (n)
+    {
+        case 1://shoot
+            cout << shoot(2/3,2/3);
+            break;
+        case 2://rrh
+            cout << shootrrh(2/3,2/3);
+            break;
+        case 3://rrw
+            cout <<  shootrrw(2/3,2/3);
+            break;
+        case 4://rrb
+            cout << shootrrb(2/3,2/3);
+            break;
+        default:
+            break;
+    }
+    return 0;
 }
---
+++ mathcore.h
@@ -0,0 +1,11 @@
+//
+// Created by cooln on 20/05/2025.
+//
+
+#ifndef MATHCORE_H
+#define MATHCORE_H
+double shoot(double x, double y);
+double shootrrh(double x, double y);
+double shootrrw(double x, double y);
+double shootrrb(double x, double y);
+#endif //MATHCORE_H
